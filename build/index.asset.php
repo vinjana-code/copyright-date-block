8427e6370ffa8abd3f56618e4e84c4903d0cd14a
vinjana-code <vineeshjana@gmail.com>
2024-11-18 12:01:53 -0500

    Initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ed71168102fc88884adf');
---
+++ copyright-date-block.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name:       Copyright Date Block
+ * Description:       Example block scaffolded with Create Block tool.
+ * Requires at least: 6.6
+ * Requires PHP:      7.2
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       copyright-date-block
+ *
+ * @package copyright-date
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+add_action( 'init', 'create_copyright_date_block_block_init' );
+function create_copyright_date_block_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+
